8ee023c9017323684aeba856dd063ff64d74c643
MacApos <maciejapostol98@gmail.com>
2026-03-26 07:30:29 +0100

    first commit

---
+++ src/main/java/com/Main.java
@@ -0,0 +1,10 @@
+package com;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        new HashMap<>();
+    }
+}
